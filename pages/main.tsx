1001db9dc141a98ae52e681bad87afc4c55b2e4a
Duncan Applegarth <duncana513@gmail.com>
2022-04-09 10:34:45 -0700

    scaffold main pages

---
+++ pages/main.tsx
@@ -0,0 +1,7 @@
+import { NextPage } from "next";
+
+const Main: NextPage = () => {
+  return <h1>MAIN PAGE!!!</h1>;
+};
+
+export default Main;
---
+++ pages/register.tsx
@@ -0,0 +1,7 @@
+import { NextPage } from "next";
+
+const Register: NextPage = () => {
+  return <h1>REGISTER PAGE!!</h1>;
+};
+
+export default Register;
