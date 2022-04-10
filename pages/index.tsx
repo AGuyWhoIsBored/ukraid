1ddd4b5757685c6faffad66bc414afa529de1178
edcor <edcorriea@gmail.com>
2022-04-10 01:53:36 -0700

    stuff home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from "next";
+import Link from "next/link";
 
 const Home: NextPage = () => {
   return (
@@ -6,19 +7,36 @@ const Home: NextPage = () => {
       className="hero min-h-screen"
       style={{
         backgroundImage:
-          "url(https://api.lorem.space/image/fashion?w=1000&h=800)",
+          "url(https://picsum.photos/1000/800)",
       }}
     >
       <div className="hero-overlay bg-opacity-60"></div>
       <div className="hero-content text-center text-neutral-content">
-        <div className="max-w-md">
-          <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-          <p className="mb-5">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
-          </p>
-          <button className="btn btn-primary">Get Started</button>
+        <div className="max-w ">
+          <h1 className="mb-10 text-5xl font-bold">WELCOME</h1>
+          <h1 className="mb-10 text-3xl font-bold">Here are some useful links</h1>
+          <Link href={"https://www.globalgiving.org/projects/ukraine-crisis-relief-fund/"}>
+            <a className="hover:bg-slate-400/30 text-blue-600 font-bold text-4xl">Ukraine Crisis Relief Fund</a>
+          </Link>
+          <br></br>
+          <br></br>
+          <br></br>
+          <table>
+            <tr>
+              <td>
+                <Link href={"https://www.smithsonianmag.com/history/the-20th-century-history-behind-russias-invasion-of-ukraine-180979672/"}>
+                  <a className="hover:bg-slate-400/30 text-blue-600 font-bold text-4xl mr-10">Brief History of Ukraine and Russia</a>
+                </Link>
+              </td>
+              <td>
+                <Link href={"https://www.britannica.com/place/Ukraine/Economy"}>
+                  <a className="hover:bg-slate-400/30 text-blue-600 font-bold text-4xl ml-10 ">Information on Ukraine</a>
+                </Link>
+              </td>
+              <td>
+              </td>
+            </tr>
+          </table>
         </div>
       </div>
     </div>
